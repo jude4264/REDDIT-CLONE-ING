34e3938eb704dcbb521ceae5068ce8781562b6d2
jude4264 <jude4264@gmail.com>
2023-02-24 15:12:44 +0900

    [UPDATE] 커뮤니티 목록 생성

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,14 +1,28 @@
+import axios from 'axios';
 import Link from 'next/link'
-import React from 'react'
-import { useAuthState } from '../context/auth'
+import React, { Fragment } from 'react'
+import { useAuthState, useAuthDispatch } from '../context/auth'
 
 const Navbar : React.FC = () => {
 
   const {loading, authenticated} = useAuthState();
+  const dispatch = useAuthDispatch()
+
+  const handleLogout = () =>{
+
+    axios.post("/auth/logout")
+    .then(()=>{ 
+      dispatch("LOGOUT");
+      window.location.reload();
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
+  }
 
 
   return (
-    <div className='fixed inset-x-0 top-0 z-10 flex item-center justify-between h-16 px-5 bg-white'>
+    <div className='fixed inset-x-0 top-0 z-10 flex items-center justify-between h-16 px-5 bg-white'>
       <span className='text-2xl font-semibold text-gray-400'>
         <Link href="/">Community</Link>
       </span>
@@ -25,11 +39,25 @@ const Navbar : React.FC = () => {
       <div className='flex'>
         {!loading&& (
           authenticated?(
-            <button className='w-20 p-2 text-center text-white bg-gray-400 rounded'>
+            <button 
+              className='w-20 p-2 text-center text-white bg-gray-400 rounded'
+              onClick={handleLogout}
+              >
               로그아웃
-            </button>) 
+            </button >) 
             : 
-            (<Fragment></Fragment>)
+            (<Fragment>
+              <Link href="/login" className='w-20 p-2 mr-2 text-center text-blue-500 border border-blue-500 rounded'>
+                  {/* <a className='w-20 p-2 mr-2 text-center text-blue-500 border border-blue-500 rounded'> */}
+                    로그인
+                  {/* </a> */}
+              </Link>
+              <Link href='/register' className='w-20 p-2 text-center text-white bg-gray-400 rounded'>
+                {/* <a className='w-20 p-2 text-center text-white bg-gray-400 rounded'> */}
+                  회원가입
+                {/* </a> */}
+              </Link>
+            </Fragment>)
         )}
 
       </div>
